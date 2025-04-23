2836a42c5496ab3f3de9d8c8cb0231c941c35a81
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-23 16:15:13 +0600

    iteration with for each

--- src/ForEach/Iteration.java
+++ src/ForEach/Iteration.java
@@ -1,5 +1,7 @@
 package ForEach;
 
+import java.util.ArrayList;
+
 public class Iteration {
 
     public static void printAll(int[] array)
@@ -25,9 +27,30 @@ public class Iteration {
         System.out.println( "\nMax value "+ max);
     }
 
+    public static void arrayList(ArrayList<Integer> array)
+    {
+        int min = Integer.MIN_VALUE; //minimum integer value;
+
+        for(int value : array) // all element of array or collection
+        {
+            if(value > min) // check current value is grater than min value;
+            {
+                min = value; //update min value
+            }
+        }
+        System.out.println(min+ " max");
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,2,4,6,7};
         printAll(arr);
         findMax(arr);
+
+        ArrayList<Integer> arrL =  new ArrayList<>();
+        arrL.add(5);
+        arrL.add(6);
+        arrL.add(8);
+        arrL.add(10);
+        arrayList(arrL);
     }
 }
