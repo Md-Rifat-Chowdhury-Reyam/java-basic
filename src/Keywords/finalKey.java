d7093521ac89321dfb00b7b42ff1a7838007fc18
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-24 07:43:09 +0600

    final keyword update

--- src/Keywords/finalKey.java
+++ src/Keywords/finalKey.java
@@ -3,7 +3,7 @@ package Keywords;
 public class finalKey {
 
     public static void main(String[] args) {
-
+//Indicates that a variable holds a constant value or that a method will not be overridden
         final int i = 10;  // final key for fixed value for the variable;
         //if we try to assign to i more value compile will show error;
 
