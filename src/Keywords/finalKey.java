e393ebdb5f0d8d89ba8ea5c98887ce2411599b7d
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-24 07:57:47 +0600

    final keyword update

--- src/Keywords/finalKey.java
+++ src/Keywords/finalKey.java
@@ -5,7 +5,7 @@ public class finalKey {
     public static void main(String[] args) {
 //Indicates that a variable holds a constant value or that a method will not be overridden
         final int i = 10;  // final key for fixed value for the variable;
-        //if we try to assign to i more value compile will show error;
+        //if we try to assign to i more value compile will show compile error;
 
 
         if(i == 10)
