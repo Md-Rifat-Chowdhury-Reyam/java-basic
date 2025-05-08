37b2a50d4b8118ed252a66edf5e6249cc26983e8
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-08 12:16:41 +0600

    switch case with string

---
+++ src/ControlFlow/ConditionalStatement/SwitchCase/Example2.java
@@ -0,0 +1,38 @@
+package ControlFlow.ConditionalStatement.SwitchCase;
+
+import java.util.Scanner;
+
+public class Example2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Chose your day in number : ");
+        int countDay = sc.nextInt();
+        
+        String day;
+
+        switch (countDay)
+        {
+            case 1:
+                day = "Saturday";
+                break;
+
+            case 2:
+                day = "Sunday";
+                break;
+
+            case 3:
+                day = "Monday";
+                break;
+
+            default:
+                day = "Friday";
+                break;
+
+
+        }
+        System.out.println(day);
+
+
+    }
+}
