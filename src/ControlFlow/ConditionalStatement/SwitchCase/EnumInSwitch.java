d5df006bc1ad2528f285f03c979ea180e461a8c4
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-08 13:18:49 +0600

    variable operation  switch case

--- src/ControlFlow/ConditionalStatement/SwitchCase/EnumInSwitch.java
+++ src/ControlFlow/ConditionalStatement/SwitchCase/EnumInSwitch.java
@@ -2,14 +2,16 @@ package ControlFlow.ConditionalStatement.SwitchCase;
 
 public class EnumInSwitch {
 
-    public enum dayData { Sun, Mon, Tue };
+    //enum
+    public enum dayData { Sun, Mon, Tue }; //so here dayData act as a data type
+
     public static void main(String[] args) {
 
-        dayData[] dayName = dayData.values();
+        dayData[] dayVal = dayData.values();
         String day;
         int count = 0;
 
-        for (dayData data : dayName )
+        for (dayData data : dayVal )
         {
             switch (data)
             {
--- src/ControlFlow/ConditionalStatement/SwitchCase/Example1.java
+++ src/ControlFlow/ConditionalStatement/SwitchCase/Example1.java
@@ -5,7 +5,7 @@ public class Example1 {
     public static void main(String[] args) {
         int i = 4;
 
-       switch (i)
+       switch (i + 1) // we can use like switch ( i + 1 ) that means we can do operation with variable;
        {
            case 1:
                System.out.println("I am one");
@@ -18,6 +18,16 @@ public class Example1 {
            case 3:
                System.out.println("I am three");
                break;
+
+           case 3 + 1:
+               System.out.println(" I am four");
+               break;
+
+// Error: java: constant expression required  so in the case we can use only value operation not variable operation
+//           case i + 1:
+//               System.out.println("I am five but compiler show error");
+//               break;
+
            default:
                System.out.println("I am default");
 
