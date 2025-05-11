62aa552156c2caa13ba0da6433e25d6d540819e3
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-11 13:33:27 +0600

    Predefined and static method

---
+++ src/Methods/Predefined/Predefined.java
@@ -0,0 +1,11 @@
+package Methods.Predefined;
+
+public class Predefined {
+
+    public static void main(String[] args) {
+        Predefined pr = new Predefined();
+
+        System.out.println(pr.hashCode());
+    }
+
+}
---
+++ src/Methods/Static/Example1.java
@@ -0,0 +1,17 @@
+package Methods.Static;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Example1 {
+
+    static void hello()
+    {
+        System.out.println("Hello Rifat");
+    }
+
+    public static void main(String[] args) {
+
+        Example1.hello();
+    }
+
+}
