03fe15aa079728a2df2cf226992383f84d825eb9
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-15 13:43:13 +0600

    StringBuffer

---
+++ src/String/StringBuffer/AppendM.java
@@ -0,0 +1,14 @@
+package String.StringBuffer;
+
+public class AppendM {
+    public static void main(String[] args) {
+
+       StringBuffer sb = new StringBuffer();
+
+       sb.append("Md. ");
+       sb.append("Rifat");
+       sb.append(" Chowdhury");
+
+       System.out.println(sb.toString());
+    }
+}
